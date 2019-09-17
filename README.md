82f35be420d440c04df4e5f7b50c0abc9a3fa91c
Scott Colegrove <colegrove_scott@network.lilly.com>
2019-09-17 12:18:10 -0400

    Removing  and auto loading styles

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 This is a web extension intended to allow you to override the css on specific domains.
 
+## Setup
+
+- copy `addon@scottco.co.json` file into `/Library/Application Support/Mozilla/ManagedStorage/`
+
 ## TODO:
 
+- Verify if es6 can be used or not
 - Inject custom css onto a domain
 - Blacklist specific rules on a site
 - In-extension editor
--- background.js
+++ background.js
@@ -1,68 +1,60 @@
-const TITLE_APPLY = "Apply CSS";
-const TITLE_REMOVE = "Remove CSS";
-const APPLICABLE_PROTOCOLS = ["http:", "https:"];
+// const TITLE_APPLY = "Apply CSS";
+// const TITLE_REMOVE = "Remove CSS";
+// const APPLICABLE_PROTOCOLS = ["http:", "https:"];
 
 /*
 Toggle CSS: based on the current title, insert or remove the CSS.
 Update the page action's title and icon to reflect its state.
 */
-function toggleCSS(tab) {
+// function toggleCSS(tab) {
+//   function gotTitle(title) {
+//     if (title === TITLE_APPLY) {
+//       // browser.pageAction.setIcon({tabId: tab.id, path: "icons/on.svg"});
+//       // browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
+//       browser.tabs.insertCSS({file: "resources/jira.css"});
+//     } else {
+//       // browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
+//       // browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
+//       browser.tabs.removeCSS({file: "resources/jira.css"});
+//     }
+//   }
 
-  function gotTitle(title) {
-    if (title === TITLE_APPLY) {
-      browser.pageAction.setIcon({tabId: tab.id, path: "icons/on.svg"});
-      browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
-      browser.tabs.insertCSS({file: "resources/jira.css"});
-    } else {
-      browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
-      browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
-      browser.tabs.removeCSS({file: "resources/jira.css"});
+//   // var gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
+//   // gettingTitle.then(gotTitle);
+// }
+
+
+const debug = (msg) => {
+  console.log(`css-override-web-extension: ${msg}`);
+};
+
+const getExtensionData = () => {
+  return new Promise(resolve => {
+    const getExtensionStorage = browser.storage.managed.get('sites');
+    getExtensionStorage.then((res) => {
+      resolve(res.sites);
+    });
+  });
+};
+
+const initializeTabStyles = async function(siteData) {
+    const getAllTabs = browser.tabs.query({});
+    getAllTabs.then((tabs) => {
+    for (let tab of tabs) {
+      const tabUrl = new URL(tab.url).hostname;
+      if (siteData[tabUrl] && siteData[tabUrl].enabled) {
+        debug(`Applying custom styles to ${tabUrl}`);
+        browser.tabs.insertCSS(tab.id, {file: "resources/jira.css"});
+      }
     }
-  }
-
-  var gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
-  gettingTitle.then(gotTitle);
+  });
 }
 
-/*
-Returns true only if the URL's protocol is in APPLICABLE_PROTOCOLS.
-*/
-function protocolIsApplicable(url) {
-  var anchor =  document.createElement('a');
-  anchor.href = url;
-  return APPLICABLE_PROTOCOLS.includes(anchor.protocol);
-}
+const initializeExtension = async function() {
+  debug('Initializing extension');
 
-/*
-Initialize the page action: set icon and title, then show.
-Only operates on tabs whose URL's protocol is applicable.
-*/
-function initializePageAction(tab) {
-  if (protocolIsApplicable(tab.url)) {
-    browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
-    browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
-    browser.pageAction.show(tab.id);
-  }
-}
+  let siteData = await getExtensionData();
+  await initializeTabStyles(siteData);
+};
 
-/*
-When first loaded, initialize the page action for all tabs.
-*/
-var gettingAllTabs = browser.tabs.query({});
-gettingAllTabs.then((tabs) => {
-  for (let tab of tabs) {
-    initializePageAction(tab);
-  }
-});
-
-/*
-Each time a tab is updated, reset the page action for that tab.
-*/
-browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  initializePageAction(tab);
-});
-
-/*
-Toggle CSS when the page action is clicked.
-*/
-browser.pageAction.onClicked.addListener(toggleCSS);
+initializeExtension();
