be1a9aafe88fae4f9e9435249b6e6c96723bd6dd
Scott Colegrove <colegrove_scott@network.lilly.com>
2019-09-19 14:22:00 -0400

    Make popup look nicer

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,8 @@ const debug = (msg) => {
   console.log(`css-override-web-extension: ${msg}`);
 };
 
+const getTabUrl = (tab) => new URL(tab.url).hostname;
+
 const getActiveTab = () => {
   return browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
     return tabs[0];
@@ -18,24 +20,35 @@ const setStorageData = (obj) => {
   return browser.storage.sync.set(obj).then(() => true, () => false);
 }
 
+const updatePopup = (enabled) => {
+  const btnToggle = document.getElementById("btnToggle");
+  btnToggle.innerHTML = enabled ? 'ON' : 'OFF';
+  if (enabled) {
+    btnToggle.classList.remove('off');
+  } else {
+    btnToggle.classList.add('off');
+  }
+}
+
 const toggleStyles = async function() {
+  debug('Toggle style')
   let activeTab = await getActiveTab();
-  const tabUrl = new URL(activeTab.url).hostname;
+  const tabUrl = getTabUrl(activeTab);
   if (!tabUrl) {
     return;
   }
 
   let tabData = await getStorageData(tabUrl);
+  let enabled = true;
   if (tabData && tabData[tabUrl]) {
-    console.log('toggle')
-    console.log('tab', tabData)
     tabData[tabUrl].enabled = !tabData[tabUrl].enabled;
+    enabled = tabData[tabUrl].enabled;
     await setStorageData(tabData);
   } else {
-    console.log('set initial')
+    debug('Applying styles to site for first time')
     let obj = {};
     obj[tabUrl] = {
-      enabled: true
+      enabled
     };
     await setStorageData(obj);
   }
@@ -47,11 +60,25 @@ const toggleStyles = async function() {
       url: tabUrl,
     },
   }).then((message) => {
-
+    updatePopup(enabled);
   });
 }
+
+const initializePopup = async function() {
+  let activeTab = await getActiveTab();
+  const tabUrl = getTabUrl(activeTab);
+
+  let tabData = await getStorageData(tabUrl);
+  let enabled = true;
+  if (tabData && tabData[tabUrl]) {
+    updatePopup(tabData[tabUrl].enabled);
+  } else {
+    updatePopup(false);
+  }
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const btnToggle = document.getElementById("btnToggle");
   btnToggle.addEventListener('click', toggleStyles);
+  initializePopup();
 });
