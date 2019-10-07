e4813c97c1b770bf8479a9a97ecfe6bd470294af
Scott Colegrove <colegrove_scott@network.lilly.com>
2019-10-07 12:48:21 -0400

    More lint fix

--- background.js
+++ background.js
@@ -1,69 +1,71 @@
 const debug = (msg) => {
-  console.log(`css-override-web-extension: ${msg}`);
+  console.log(`css-override-web-extension: ${msg}`); // eslint-disable-line
 };
 
-const getStorageData = (domainKey) => {
-  return browser.storage.sync.get(domainKey);
-};
+const getStorageData = (domainKey) => browser.storage.sync.get(domainKey);
 
 const getTabUrl = (tab) => new URL(tab.url).hostname;
 
 const toggleCSS = (tabId, enabled, style) => {
   if (enabled) {
-    browser.tabs.insertCSS(tabId, {code: style});
+    browser.tabs.insertCSS(tabId, { code: style });
   } else {
-    browser.tabs.removeCSS(tabId, {code: style});
+    browser.tabs.removeCSS(tabId, { code: style });
   }
 };
 
 const setTabStyle = (id, url) => {
   getStorageData(url).then((tabData) => {
-    debug(JSON.stringify(tabData))
+    debug(JSON.stringify(tabData));
     if (tabData[url]) {
-      const enabled = tabData[url].enabled;
-      toggleCSS(id, enabled, tabData[url].style)
+      const { enabled } = tabData[url];
+      toggleCSS(id, enabled, tabData[url].style);
     }
   });
-}
+};
 
 const initializeTabStyles = () => {
-    const getAllTabs = browser.tabs.query({});
-    getAllTabs.then((tabs) => {
+  const getAllTabs = browser.tabs.query({});
+  getAllTabs.then((tabs) => {
+  /* eslint-disable no-restricted-syntax */
     for (const tab of tabs) {
       const tabUrl = getTabUrl(tab);
-      setTabStyle(tab.id, tabUrl)
+      setTabStyle(tab.id, tabUrl);
     }
+  /* eslint-enable no-restricted-syntax */
   });
-}
+};
 
 const reloadTab = (tab) => {
   debug('Reloading tab');
-  setTabStyle(tab.id, tab.url)
+  setTabStyle(tab.id, tab.url);
 };
 
 const tabStyleChanged = (urlToUpdate) => {
   const getAllTabs = browser.tabs.query({});
-    getAllTabs.then((tabs) => {
+  getAllTabs.then((tabs) => {
+    /* eslint-disable no-restricted-syntax */
     for (const tab of tabs) {
       const tabUrl = getTabUrl(tab);
       if (tabUrl === urlToUpdate) {
         browser.tabs.reload(tab.id);
       }
     }
+    /* eslint-enable no-restricted-syntax */
   });
-}
+};
 
 // Listen for actions passed from popup window
 const eventListener = (request, sender, sendResponse) => {
   debug(`Event listener action ${request.action}`);
   if (request.action === 'reloadTab') {
     reloadTab(request.tab);
-    sendResponse({response: true});
+    sendResponse({ response: true });
   } else if (request.action === 'updateStyle') {
     // if styles for a url changed - update all its tabs
     tabStyleChanged(request.tabUrl);
   }
-}
+};
 
 const initializeExtension = () => {
   debug('Initializing extension');
@@ -72,7 +74,7 @@ const initializeExtension = () => {
   browser.runtime.onMessage.addListener(eventListener);
   browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
     // Re apply styles if tab is updated (refresh)
-    reloadTab({id, url: getTabUrl(tab)});
+    reloadTab({ id, url: getTabUrl(tab) });
   });
 };
 
